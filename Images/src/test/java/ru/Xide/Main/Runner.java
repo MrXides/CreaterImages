89d3515ea2251f32a8259389ae29096b07b55bb5
sergey.i.smirnov@rt.ru <78102903+MrXides@users.noreply.github.com>
2022-10-11 21:01:49 +0300

    newImage

--- Images/src/test/java/ru/Xide/Main/Runner.java
+++ Images/src/test/java/ru/Xide/Main/Runner.java
@@ -35,7 +35,7 @@ public class Runner {
         mainInput.click();
         mainInput.sendKeys("Cat");
 
-        int i = 850;
+        int i = 100;
         while (i >= 0) {
 
         WebElement lineArt = driver.findElement(By.xpath("//*[@id=\"blur-overlay\"]/div/div/div[1]/div[1]/div[1]/div[2]/div/div[2]/div[12]/div/div/img"));
@@ -53,4 +53,46 @@ public class Runner {
         }
         driver.quit();
     }
+
+    @Test
+    public void newNero() throws InterruptedException {
+
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(false); // визуализация
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chrome/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("https://www.wombo.art/create");
+
+        String input = "//*[@id=\"blur-overlay\"]/div/div/div[1]/div[1]/div[1]/div[1]/div[1]/input";
+
+        WebElement mainInput = driver.findElement(By.xpath(input));
+        mainInput.click();
+        mainInput.sendKeys("Dog");
+
+        int i = 100;
+        while (i >= 0) {
+
+            WebElement lineArt = driver.findElement(By.xpath("//*[@id=\"blur-overlay\"]/div/div/div[1]/div[1]/div[1]/div[2]/div/div[2]/div[12]/div/div/img"));
+            lineArt.click();
+            driver.findElement(By.xpath("//*[@id=\"blur-overlay\"]/div/div/div[1]/div[2]/button")).click();
+
+            new WebDriverWait(driver, 3000).
+                    until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"blur-overlay\"]/div/div/div[2]/div/div[2]/button/div/div")));
+
+            driver.findElement(By.xpath("//*[@id=\"blur-overlay\"]/div/div/div[2]/div/div[2]/button/div/div")).click();
+
+            driver.findElement(By.xpath("//*[@id=\"blur-overlay\"]/div/div/div/div[1]/div/button")).click();
+            Thread.sleep(2000);
+            i--;
+        }
+        driver.quit();
+    }
+
 }
